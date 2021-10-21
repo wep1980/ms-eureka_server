27a73e720daba0b2000f73b5c197d6fc0c62bee1
wep1980 <wepbike@gmail.com>
2021-10-21 20:53:54 -0300

    Configurando eureka-Server

--- src/main/java/br/com/wepdev/eurekaserver/EurekaServerApplication.java
+++ src/main/java/br/com/wepdev/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,9 @@ package br.com.wepdev.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // Habilitando o eureka server. PARA TESTAR APOS SUBIR A APLICAÇÃO TESTAR COM A URL localhost:8761
 @SpringBootApplication
 public class EurekaServerApplication {
 
